c85f44cad5a635b93f39a3bd0856fb742d224412
kalinskiy82 <110938560+kalinskiy82@users.noreply.github.com>
2025-11-28 15:42:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import random
 import logging
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, imghdr
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, update
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext
 from datetime import time
 import pytz
